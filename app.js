389371b25d3e80135e2a3cd90c2009c50215e18c
InvalidSB <invalidSB45@gmail.com>
2021-09-08 00:52:15 +0545

    make it better

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ mongoose
 if(process.env.NODE_ENV =="prodcution"){
   app.use(express.static('client/build'))
   const path= require("path")
-  app.get("*",(req,res)=>{
+  app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build',"index.html"))
   })
 }
