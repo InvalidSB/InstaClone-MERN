6dd54a4b1d70abdd702cf35133e1dd407980026f
InvalidSB <invalidSB45@gmail.com>
2021-09-08 00:31:19 +0545

    make it better

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ if(process.env.NODE_ENV =="prodcution"){
   app.use(express.static('client/build'))
   const path= require("path")
   app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"client",'build',"inddex.html"))
+    res.sendFile(path.resolve(__dirname,'client','build',"index.html"))
   })
 }
 
